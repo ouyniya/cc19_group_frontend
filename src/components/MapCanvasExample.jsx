a673814274d05e3772097334fa94964b5db743f1
niya-s <niya.somkerd@gmail.com>
2025-03-17 07:39:26 +0700

    ✨ feat: add map canvas

---
+++ src/components/MapCanvasExample.jsx
@@ -0,0 +1,74 @@
+import {
+  LayersControl,
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { useState } from "react";
+import BaseMap from "../components/layer/BaseMap";
+import Province from "../components/layer/Province";
+
+function MapCanvasExample() {
+  function ClickHandler({ onClick }) {
+    useMapEvents({
+      click: (e) => {
+        onClick(e.latlng);
+        console.log(e.latlng);
+      },
+    });
+    return null;
+  }
+
+  const [position, setPosition] = useState(null);
+
+  return (
+    <div>
+      <MapContainer
+        style={{ height: "400px", width: "560px" }}
+        center={[15, 101]}
+        zoom={6}
+      >
+        <BaseMap />
+        {/* <LocationMarker /> */}
+        {position && (
+          <Marker position={position}>
+            <Popup>
+              You clicked here: <br /> {position.lat.toFixed(5)},{" "}
+              {position.lng.toFixed(5)}
+            </Popup>
+          </Marker>
+        )}
+
+        {/* <Marker position={[13.5, 101]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker position={[15, 101]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker> */}
+
+        {/* <Province /> */}
+        {/* Click event handler */}
+        <ClickHandler onClick={(latlng) => setPosition(latlng)} />
+      </MapContainer>
+
+      {/* {position && (
+        <div
+          style={{ marginTop: "10px", padding: "10px", borderRadius: "5px" }}
+        >
+          <strong>Clicked Position:</strong> <br />
+          Latitude: {position.lat}, Longitude: {position.lng}
+        </div>
+      )} */}
+    </div>
+  );
+}
+
+export default MapCanvasExample;
