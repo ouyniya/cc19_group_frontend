339e37b7e8bb16aebb14f866dd12310031a47de8
niya-s <niya.somkerd@gmail.com>
2025-03-17 08:43:02 +0700

    ✨ feat: add select box of province and ditricts

---
+++ src/api/locationApi.jsx
@@ -0,0 +1,17 @@
+import { axios, getAccessToken } from "../configs/axiosInstance";
+
+const locationApi = {};
+
+locationApi.actionGetProvince = async () => {
+  return axios.get(`/api/location/province`, {
+    headers: { Authorization: `Bearer ${getAccessToken()}` },
+  });
+};
+
+locationApi.actionGetDistrict = async (provinceId) => {
+  return axios.get(`/api/location/district?provinceId=${provinceId}`, {
+    headers: { Authorization: `Bearer ${getAccessToken()}` },
+  });
+};
+
+export default locationApi;
