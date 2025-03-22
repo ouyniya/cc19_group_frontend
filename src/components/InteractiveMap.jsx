e905c87e6ba3720b836c0278c13f02fd849f9ee7
Misboonyos <boonyos.k@gmail.com>
2025-03-22 11:50:00 +0700

    ✨ feat:add data

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -15,7 +15,7 @@ import Province from "../components/layer/Province";
 
 
 // Custom marker icons
-const createCustomIcon = (iconUrl, size = [25, 25]) => {
+const createCustomIcon = (iconUrl, size = [50, 50]) => {
   return L.icon({
     iconUrl,
     iconSize: size,
@@ -27,7 +27,7 @@ const createCustomIcon = (iconUrl, size = [25, 25]) => {
 // Default icons for different place types
 const defaultIcons = {
   attraction: createCustomIcon("https://cdn-icons-png.flaticon.com/128/3066/3066644.png"),
-  eatery: createCustomIcon("https://cdn-icons-png.flaticon.com/128/16455/16455487.png"),
+  eatery: createCustomIcon("https://cdn-icons-png.flaticon.com/128/10914/10914008.png"),
   shopping: createCustomIcon("https://cdn-icons-png.flaticon.com/128/3081/3081648.png"),
   nature: createCustomIcon("https://cdn-icons-png.flaticon.com/128/3175/3175209.png"),
   hotel: createCustomIcon("https://cdn-icons-png.flaticon.com/128/594/594106.png"),
@@ -125,12 +125,12 @@ const placesData = [
     highlights: ["The only one rock salt pond in the world", "The ancient rock salt pond"],
     address: "Bo Kluea Tai, Bo Kluea District, Nan, Thailand",
     nearbyAttractions: [
-      { id: 103, name: "Ton Mang Klang Na Camping", position: [19.159880, 101.158347], type: "attraction", distance: 1.1, image: "https://scontent.fbkk6-1.fna.fbcdn.net/v/t39.30808-6/348703339_737363335062481_2042458407198846152_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGyBNJYuV5K9aOUx-C4CI7OzO5V71DbvQPM7lXvUNu9A7Jf429KZYJOZge3Id9OnCw&_nc_ohc=2bWP--d09nUQ7kNvgERMGDo&_nc_oc=AdmZGiNbA7gygvgeP-tr_gGKz_8PArbejDlRUu5L1j0A0C_pzVpeCugujVOjxa2Onf0&_nc_zt=23&_nc_ht=scontent.fbkk6-1.fna&_nc_gid=tNYUjdfYT4De4GYisYKY7A&oh=00_AYHngwdAeESy9O0ftDB4denm-4Q5jTjYYPn5kJDurogasQ&oe=67E2A6E3" },
-      { id: 104, name: "Baanna Kham Salt Maker", position: [19.149452, 101.157014], type: "attraction", distance: 1.8, image: "https://resource.nationtv.tv/resource/photo_news/2021/02/15/640_5jfbe6abd658aeh6a6j5b.jpg?x-image-process=style/lg-webp" },
+      { id: 105, name: "Ton Mang Klang Na Camping", position: [19.159880, 101.158347], type: "attraction", distance: 1.1, image: "https://scontent.fbkk6-1.fna.fbcdn.net/v/t39.30808-6/348703339_737363335062481_2042458407198846152_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=108&ccb=1-7&_nc_sid=cc71e4&_nc_eui2=AeGyBNJYuV5K9aOUx-C4CI7OzO5V71DbvQPM7lXvUNu9A7Jf429KZYJOZge3Id9OnCw&_nc_ohc=2bWP--d09nUQ7kNvgERMGDo&_nc_oc=AdmZGiNbA7gygvgeP-tr_gGKz_8PArbejDlRUu5L1j0A0C_pzVpeCugujVOjxa2Onf0&_nc_zt=23&_nc_ht=scontent.fbkk6-1.fna&_nc_gid=tNYUjdfYT4De4GYisYKY7A&oh=00_AYHngwdAeESy9O0ftDB4denm-4Q5jTjYYPn5kJDurogasQ&oe=67E2A6E3" },
+      { id: 106, name: "Baanna Kham Salt Maker", position: [19.149452, 101.157014], type: "attraction", distance: 1.8, image: "https://resource.nationtv.tv/resource/photo_news/2021/02/15/640_5jfbe6abd658aeh6a6j5b.jpg?x-image-process=style/lg-webp" },
     ],
     nearbyPlaces: [
-      { id: 203, name: "Bo Kluea Hospital", type: "hospital", price: "n/a", rating: 4.2, views: 12 },
-      { id: 204, name: "Bo Kluea District Office", type: "default", price: "n/a", rating: 3.9, views: 9 },
+      { id: 205, name: "Bo Kluea Hospital", type: "hospital", price: "n/a", rating: 4.2, views: 12 },
+      { id: 206, name: "Bo Kluea District Office", type: "default", price: "n/a", rating: 3.9, views: 9 },
     ]
   },
   {
@@ -144,12 +144,126 @@ const placesData = [
     highlights: ["The Royal Temple of King Rama IX", "The ancient lanna temple", "Fascinating architecture"],
     address: "QQ5R+7MV Muang Tuet, Phu Phiang District, Nan, Thailand",
     nearbyAttractions: [
-      { id: 103, name: "Baan Kru Kai Fabric Weave", position: [18.756747, 100.791868], type: "attraction", distance: 0.5, image: "https://mpics.mgronline.com/pics/Images/559000004113801.JPEG" },
-      { id: 104, name: "Baan Nam Ngeiw", position: [18.754629, 100.790222], type: "eatery", distance: 1.7, image: "https://img.wongnai.com/p/1968x0/2018/11/04/3ceccd73b8574f6da21e37203f62e714.jpg" },
+      { id: 107, name: "Baan Kru Kai Fabric Weave", position: [18.756747, 100.791868], type: "attraction", distance: 0.5, image: "https://mpics.mgronline.com/pics/Images/559000004113801.JPEG" },
+      { id: 108, name: "Baan Nam Ngiew", position: [18.754629, 100.790222], type: "eatery", distance: 1.7, image: "https://img.wongnai.com/p/1968x0/2018/11/04/3ceccd73b8574f6da21e37203f62e714.jpg" },
     ],
     nearbyPlaces: [
-      { id: 203, name: "Bo Kluea Hospital", type: "hospital", price: "n/a", rating: 4.2, views: 12 },
-      { id: 204, name: "Bo Kluea District Office", type: "default", price: "n/a", rating: 3.9, views: 9 },
+      { id: 207, name: "Pagoda Sight Residence", type: "hotel", price: "THB1,084", rating: 4.5, views: 113 },
+      { id: 208, name: "Wiang Kaew Hotel", type: "hotel", price: "THB1,252", rating: 4.3, views: 290 },
+    ]
+  },
+  {
+    id: 5,
+    name: "Doi Phu Wae National Park",
+    position: [19.385833, 101.116667],
+    type: "nature",
+    rating: 4.7,
+    views: 3,
+    image: "https://travel.mthai.com/app/uploads/2015/01/10599529_822619207794473_5549047632274721127_n.jpg",
+    highlights: ["A land of majestic mountains and drifting mist.", "Unique local flora and rare plant species; Khor tree and the Dok Ku Lap Phan Pi (Thai Rose of the North)"],
+    address: "Khun Nan, Chaloem Phra Kiat District, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 109, name: "Doi Phu Kha National Park", position: [19.200560, 101.080650], type: "nature", distance: 54.3, image: "https://www.thailandguidebook.com/wp-content/uploads/2012/01/IMG_1487.jpg" },
+      { id: 110, name: "Sapan Waterfall", position: [19.203184, 101.196357], type: "nature", distance: 28.9, image: "https://www.nanprovince.com/wp-content/uploads/2020/11/Sapan-Waterfall-7.jpg" },
+    ],
+    nearbyPlaces: [
+      { id: 209, name: "Pagoda Sight Residence", type: "hotel", price: "THB1,084", rating: 4.5, views: 113 },
+      { id: 210, name: "Wiang Kaew Hotel", type: "hotel", price: "THB1,252", rating: 4.3, views: 290 },
+    ]
+  },
+  {
+    id: 6,
+    name: "Tambon Silapetch Farmstay Farmerschool",
+    position: [19.108200, 100.943792],
+    type: "attraction",
+    rating: 4.7,
+    views: 3,
+    image: "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/73364256_146545126737094_5127476148606861312_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFfT1Nockrw2TtkPjp_D-mbh3MxfgETh6WHczF-AROHpZDNpWWtArYq8ANzbPolCVU&_nc_ohc=brYxinJ0ty8Q7kNvgFx8Uz2&_nc_oc=AdkHY5iZYMacejy8apcYISjPiCvjdsLwpgxNUnyj5IE6JUUl2s72Dl1X91JFzecpDB4&_nc_zt=23&_nc_ht=scontent.fbkk5-3.fna&_nc_gid=3VBgPBoJuV-EQke_58Qvgg&oh=00_AYEmFwjZN7kJU7S9U1dhtmn_CtdCRU00bauo8O7jbrZKeQ&oe=68047935",
+    highlights: ["A place to explore the art of rice cultivation through the hands-on wisdom of local villagers."],
+    address: "225 Moo 1 Baan Na Kam, Silapetch, Pua, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 111, name: "Doi Phu Kha National Park", position: [19.200560, 101.080650], type: "nature", distance: 54.3, image: "https://www.thailandguidebook.com/wp-content/uploads/2012/01/IMG_1487.jpg" },
+      { id: 112, name: "Sapan Waterfall", position: [19.203184, 101.196357], type: "nature", distance: 28.9, image: "https://www.nanprovince.com/wp-content/uploads/2020/11/Sapan-Waterfall-7.jpg" },
+    ],
+    nearbyPlaces: [
+      { id: 211, name: "Pagoda Sight Residence", type: "hotel", price: "THB1,084", rating: 4.5, views: 113 },
+      { id: 212, name: "Wiang Kaew Hotel", type: "hotel", price: "THB1,252", rating: 4.3, views: 290 },
+    ]
+  },
+  {
+    id: 7,
+    name: "Tambon Silapetch Farmstay Farmerschool",
+    position: [19.108200, 100.943792],
+    type: "attraction",
+    rating: 4.4,
+    views: 4,
+    image: "https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.6435-9/73364256_146545126737094_5127476148606861312_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFfT1Nockrw2TtkPjp_D-mbh3MxfgETh6WHczF-AROHpZDNpWWtArYq8ANzbPolCVU&_nc_ohc=brYxinJ0ty8Q7kNvgFx8Uz2&_nc_oc=AdkHY5iZYMacejy8apcYISjPiCvjdsLwpgxNUnyj5IE6JUUl2s72Dl1X91JFzecpDB4&_nc_zt=23&_nc_ht=scontent.fbkk5-3.fna&_nc_gid=3VBgPBoJuV-EQke_58Qvgg&oh=00_AYEmFwjZN7kJU7S9U1dhtmn_CtdCRU00bauo8O7jbrZKeQ&oe=68047935",
+    highlights: ["A place to explore the art of rice cultivation through the hands-on wisdom of local villagers."],
+    address: "225 Moo 1 Baan Na Kam, Silapetch, Pua, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 111, name: "Wat Na Kham", position: [19.110275, 100.946816], type: "temple", distance: 0.65, image: "https://ak-d.tripcdn.com/images/1i6392224phibxbar2BC1_W_640_0_R5_Q80.jpg?proc=source/trip" },
+      { id: 112, name: "Sila Phetch Reservoir", position: [19.112555, 100.952652], type: "attraction", distance: 1.4, image: "https://www.nanprovince.com/wp-content/uploads/2020/11/Sapan-Waterfall-7.jpg" },
+    ],
+    nearbyPlaces: [
+      { id: 211, name: "Nan Niran Resort", type: "hotel", price: "THB2,800", rating: 4.4, views: 227 },
+      { id: 212, name: "Saksila Resort", type: "hotel", price: "THB2,072", rating: 4.9, views: 22},
+    ]
+  },
+  {
+    id: 8,
+    name: "Doi Samer Dao National Park",
+    position: [18.376333, 100.827506],
+    type: "nature",
+    rating: 4.5,
+    views: 3765,
+    image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/89/fb/36/caption.jpg?w=2000&h=-1&s=1",
+    highlights: ["A popular camping site","Watching the stars at night", " Watching the sea of mist at the sunrise"],
+    address: "Sisaket, Nanoi District, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 113, name: "Si Nan National Park", position: [18.368378, 100.840017], type: "nature", distance: 3.4, image: "https://mychiangmaitour.com/wp-content/uploads/2019/04/si_nan_national_park08.jpg" },
+      // { id: 114, name: "Sila Phetch Reservoir", position: [19.112555, 100.952652], type: "attraction", distance: 1.4, image: "https://www.nanprovince.com/wp-content/uploads/2020/11/Sapan-Waterfall-7.jpg" },
+    ],
+    nearbyPlaces: [
+      // { id: 213, name: "Nan Niran Resort", type: "hotel", price: "THB2,800", rating: 4.4, views: 227 },
+      // { id: 214, name: "Saksila Resort", type: "hotel", price: "THB2,072", rating: 4.9, views: 22},
+    ]
+  },
+  {
+    id: 9,
+    name: "Phu Fa Pattana (Development) Center Accommodation",
+    position: [19.024851, 101.205370],
+    type: "attraction",
+    rating: 4.7,
+    views: 35,
+    image: "https://ak-d.tripcdn.com/images/1mk3u2234c2mi3ots4C26_C_1200_800_Q70.jpg?proc=source%2ftrip&proc=source%2ftrip",
+    highlights: ["A Royal Project of HRH Princess Maha Chakri Sirindhorn", "A conservation-focused destination offering opportunities to experience natural wonders and immerse in local culture."],
+    address: "155 Moo 3 Phu Fa, Bo Kluea District, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 115, name: "Phu Fa Pattana Palace", position: [19.019193, 101.210810], type: "attraction", distance: 0.75, image: "https://ak-d.tripcdn.com/images/0HJ1312000hc4inlx4AE1.jpg" },
+      { id: 116, name: "Mlabri Tribe Village, Nan", position: [19.016107, 101.210666], type: "attraction", distance: 0.9, image: "https://static.wixstatic.com/media/85540c_77cc35b438bc4a41b49f9352a08030a1~mv2.jpg/v1/fill/w_959,h_958,al_c,q_85,enc_avif,quality_auto/Mlabri%20Village.jpg" },
+    ],
+    nearbyPlaces: [
+      { id: 215, name: "Arbuya Homestay", type: "hotel", price: "THB3,500", rating: 4.9, views: 9 },
+      { id: 216, name: "Naamwah Rafting", type: "attraction", price: "THB3,500", rating: 4.9, views: 12},
+    ]
+  },
+  {
+    id: 10,
+    name: "Walking Street, Wat Phumin",
+    position: [18.775341, 100.771599],
+    type: "eatery",
+    rating: 4.1,
+    views: 33,
+    image: "https://img.wongnai.com/p/1920x0/2023/01/07/25d994d81885413e959ec5948a317793.jpg",
+    highlights: ["A lively street lined with food stalls, everyday goods, clothing, and souvenirs."],
+    address: "QQGC+3M6 Nai Wiang, Mueang Nan District, Nan, Thailand",
+    nearbyAttractions: [
+      { id: 117, name: "Nan National Museum", position: [18.776455, 100.770775], type: "attraction", distance:0.27, image: "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/315694480_511741720991177_5305468402889044031_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeE8IWDswltPAKFiMzNwtiSuF6YxoWfjxYAXpjGhZ-PFgAQiqzCEOrgKEmaN4yxe1Sc&_nc_ohc=1dOZ1ZdIk9EQ7kNvgHR0jli&_nc_oc=Adlp4Kzmewo7kJpIcaZJzw1aCWcPVUpsfvEDuYJVhOplnfH17RyQF0r77eonW_jCfjk&_nc_zt=23&_nc_ht=scontent.fbkk5-4.fna&_nc_gid=3gYlrTfBkeccoU4lUU2aqg&oh=00_AYHaaYP2jzGmsusuO4WxGHcVUQcrk9R07hULudVR4Rf4XA&oe=67E2EC1B" },
+      { id: 118, name: "Wat Phumin", position: [18.775141, 100.771615], type: "temple", distance: 0.16, image: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/07/ec/36/2c/caption.jpg?w=1200&h=-1&s=1" },
+    ],
+    nearbyPlaces: [
+      { id: 217, name: "Nan Nakara Hotel", type: "hotel", price: "THB1,172", rating: 4.5, views: 517 },
+      { id: 218, name: "Bua Place Nan", type: "hotel", price: "THB920", rating: 4.9, views: 46},
     ]
   },
 ];
@@ -419,6 +533,7 @@ const Dropdown = ({ options, onSelect }) => {
       <Marker 
         position={place.position} 
         icon={icon}
+        iconSize={[100,100]}
         eventHandlers={{
           mouseover: () => setIsHovering(place.id),
           mouseout: () => setIsHovering(null),
@@ -456,7 +571,7 @@ const Dropdown = ({ options, onSelect }) => {
           // style={{ height: "100%", width: "100%" }}
           className="h-[400px]"
           center={[19, 101]}
-          zoom={6}
+          zoom={9}
           ref={mapRef}
         >
           <BaseMap />
@@ -499,7 +614,7 @@ const Dropdown = ({ options, onSelect }) => {
           ))}
           
           {/* Click event handler */}
-          <ClickHandler onClick={(latlng) => setPosition(latlng)} />
+          {/* <ClickHandler onClick={(latlng) => setPosition(latlng)} /> */}
         </MapContainer>
       </div>
       
@@ -507,7 +622,7 @@ const Dropdown = ({ options, onSelect }) => {
       {selectedPlace && (
         <div 
           ref={sidebarRef}
-          className="sidebar bg-white shadow-lg overflow-y-auto"
+          className="sidebar bg-white !h-[400px] shadow-lg overflow-y-auto"
           style={{ 
             width: isMobile ? "100%" : `${sidebarWidth}%`, 
             height: isMobile ? "50vh" : "100vh",
